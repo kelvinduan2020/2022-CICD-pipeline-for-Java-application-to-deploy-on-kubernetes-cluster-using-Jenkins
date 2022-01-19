51f9f5245bb1d831517bf67979e5c4205b7e353c
kelvin_tester <47585544+kelvinduan2020@users.noreply.github.com>
2022-01-19 17:01:15 +0800

    Create Bootstrap.sh

---
+++ Vagrant/Jenkins/Bootstrap.sh
@@ -0,0 +1,9 @@
+# Java
+sudo apt install default-jre default-jdk -y
+
+# Jenkins
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins -y
