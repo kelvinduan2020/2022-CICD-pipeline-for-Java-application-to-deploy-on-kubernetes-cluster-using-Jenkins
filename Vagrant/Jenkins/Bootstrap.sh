d89fe525b9692273e42aaa5eab4b16661d02b4fe
kelvin_tester <47585544+kelvinduan2020@users.noreply.github.com>
2022-01-20 10:17:04 +0800

    Update Bootstrap.sh

--- Vagrant/Jenkins/Bootstrap.sh
+++ Vagrant/Jenkins/Bootstrap.sh
@@ -1,9 +1,14 @@
 # Java
-sudo apt install default-jre default-jdk -y
+sudo apt update -y
+sudo apt install openjdk-8-jdk openjdk-8-jre -y 
+#sudo apt install openjdk-11-jdk -y
 
 # Jenkins
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
-    /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get install jenkins -y
+wget https://updates.jenkins-ci.org/download/war/2.330/jenkins.war
+nohup java -jar jenkins.jar &
+
+# Jenkins
+#wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+#sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+#sudo apt-get update
+#sudo apt-get install jenkins -y
