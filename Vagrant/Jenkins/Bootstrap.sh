61961987d8e76a4a2bd21f3c6862ebe9bc42d9e5
kelvin_tester <47585544+kelvinduan2020@users.noreply.github.com>
2022-01-20 12:01:39 +0800

    Update Bootstrap.sh

--- Vagrant/Jenkins/Bootstrap.sh
+++ Vagrant/Jenkins/Bootstrap.sh
@@ -3,12 +3,24 @@ sudo apt update -y
 sudo apt install openjdk-8-jdk openjdk-8-jre -y 
 #sudo apt install openjdk-11-jdk -y
 
-# Jenkins: https://updates.jenkins-ci.org/download/war/
+# Jenkins
 wget https://updates.jenkins-ci.org/download/war/2.330/jenkins.war
 nohup java -jar jenkins.war &
 
-# Jenkins
-#wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-#sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-#sudo apt-get update
-#sudo apt-get install jenkins -y
+# Helm & Datree.io
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+# Docker
+sudo apt-get update 
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+sudo usermod -aG docker vagrant
+newgrp docker
+
